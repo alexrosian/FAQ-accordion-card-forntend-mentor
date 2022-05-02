ef52af5538c649fa1bd6b6595b43805e7a2a0586
Alex Rosian Savu <alexandrurosiansavu@Alexs-MacBook-Air-2.local>
2022-05-02 07:44:52 +0300

    draft

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+let acc = document.getElementsByClassName('accordion');
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener('click', function () {
+    this.classList.toggle('accordion-active');
+    this.firstElementChild.classList.toggle('arrow-active');
+    let pannel = this.nextElementSibling;
+    if (pannel.style.display === 'block') {
+      pannel.style.display = 'none';
+    } else {
+      pannel.style.display = 'block';
+    }
+  });
+}
